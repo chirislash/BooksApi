458c5e2012b19be83c10b6f0e4d7d7c643875687
Luis <chirislash@outlook.com>
2020-02-29 21:15:10 +0100

    Initial commit

---
+++ BooksApi/Controllers/BooksController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksApi.Models;
+using BooksApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BooksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+        public BooksController(BookService booksService) {
+            _bookService = booksService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Book>> Get() =>
+            _bookService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetBook")]
+        public ActionResult<Book> Get(String id) {
+            var book = _bookService.Get(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Create(Book book)
+        {
+            _bookService.Create(book);
+            return CreatedAtRoute("GetBook", new { id = book.Id.ToString(), book });
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(String id, Book bookIn)
+        {
+            var book = _bookService.Get(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _bookService.Update(id,bookIn);
+            return NoContent();
+        }
+        [HttpDelete]
+        public IActionResult Delete(String id)
+        {
+            var book = _bookService.Get(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _bookService.Remove(book);
+            return NoContent();
+        }
+
+    }
+}
---
+++ BooksApi/Services/BookService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using BooksApi.Models;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings) {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() =>
+        _books.Find(book => true).ToList();
+
+        public Book Get(String Id) =>
+            _books.Find<Book>(book => book.Id == Id).FirstOrDefault();
+
+        public Book Create(Book book) {
+            _books.InsertOne(book);
+            return book;
+        } 
+
+        public void Update(String Id, Book BookIn) =>
+            _books.ReplaceOne(book => book.Id == Id, BookIn);
+
+        public void Remove(Book BookIn) =>
+            _books.DeleteOne(book => book.Id == BookIn.Id);
+
+        public void Remove(String Id) =>
+            _books.DeleteOne(book => book.Id == Id);
+        
+    }
+}
